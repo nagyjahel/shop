e3e5cd09d178e25ae7ee802b0d8b699ec11e7d4f
Nagy Jáhel <nagy.jahel@accenture.com>
2021-03-12 09:40:18 +0200

    Configured with appcenter.

--- android/app/src/main/kotlin/nagyjahel/shop/MainActivity.kt
+++ android/app/src/main/kotlin/nagyjahel/shop/MainActivity.kt
@@ -1,6 +1,17 @@
 package nagyjahel.shop
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        AppCenter.start(application, "a81d96e2-c802-4ed8-b833-54aba9587397",
+                Analytics::class.java, Crashes::class.java)
+        super.onCreate(savedInstanceState, persistentState)
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/providers/cart.dart';
@@ -5,15 +6,23 @@ import 'package:shop/providers/orders.dart';
 import 'package:shop/providers/products_provider.dart';
 import 'package:shop/screens/cart_screen.dart';
 import 'package:shop/screens/orders_screen.dart';
+import 'package:appcenter/appcenter.dart';
+import 'package:appcenter_analytics/appcenter_analytics.dart';
+import 'package:appcenter_crashes/appcenter_crashes.dart';
+import 'package:flutter/foundation.dart';
 
 import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+
 
 class MyApp extends StatelessWidget {
+
+  void main() {
+
+    runApp(MyApp());
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
